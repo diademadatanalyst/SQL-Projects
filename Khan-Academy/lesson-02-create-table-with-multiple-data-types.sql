7e79d642d3ce99a8382659cc4b07b74a2526e4f1
diademadatanalyst <diademachandra@gmail.com>
2025-12-20 23:10:16 +1100

    lesson-02-create-table-with-multiple-data-types.sql

---
+++ Khan-Academy/lesson-02-create-table-with-multiple-data-types.sql
@@ -0,0 +1,9 @@
+--Learning SQLite data types: Integer, Real, Text, and Null
+
+CREATE TABLE friends (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, weight REAL);
+
+INSERT INTO friends VALUES (1, "Jason", 26, 85.2);
+INSERT INTO friends VALUES (2, "Madonna", 47, 58.5);
+INSERT INTO friends VALUES (3, "Pumpkin", NULL, NULL);
+
+SELECT * FROM friends;
