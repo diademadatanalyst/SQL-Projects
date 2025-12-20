3946c36d4d713da221ebc4b041afe71380187d4b
diademadatanalyst <diademachandra@gmail.com>
2025-12-20 20:41:58 +1100

    lesson-01-create-table-and-inserting-data.sql

---
+++ Khan-Academy/lesson-01-create-table-and-inserting-data.sql
@@ -0,0 +1,15 @@
+--Khan Academy: Intro to SQL
+--Lesson 1: Create Tables and Inserting Data
+
+/** Grocery list:
+Bananas (4)
+Peanut Butter (1)
+Dark Chocolate Bars (2)
+**/
+
+CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
+
+INSERT INTO groceries VALUES (1, "Bananas", 4);
+INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
+INSERT INTO groceries VALUES (3, "Dark Chocolate Bars", 2);
+SELECT * FROM groceries;
